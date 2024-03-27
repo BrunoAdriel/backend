ca9f24590ad29192651bd61b90755bb42fc82d6c
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2024-03-27 18:15:00 -0300

    handlebars

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 const express = require('express')
+const handlebars = require('express-handlebars')
 
 const prodCarro = require('./routes/prod.router')
 
 const app = express()
+
+app.engine('handlebars', handlebars.engine())
+app.set('view', `${__dirname}/views`)
+app.set('view engine', 'handlebars')
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
